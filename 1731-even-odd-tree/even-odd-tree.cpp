e217bb7b30ede75f9d7e42876351ec6e6bceb77f
Pragya Choudhary <100229365+Pragyachy16@users.noreply.github.com>
2024-02-29 11:48:25 +0530

    Time: 190 ms (85.12%) | Memory: 151.9 MB (45.64%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> levelPrev;
+    
+    bool solve(TreeNode* root, int level) {
+        if(!root) {
+            return true;
+        }
+        
+        if((level%2 == 0 && root->val%2 == 0) || (level%2 != 0 && root->val%2 != 0)) {
+            return false;
+        }
+        
+        if(level >= levelPrev.size()) {
+            levelPrev.resize(level+1);
+        }
+
+        if(levelPrev[level] != 0) { //already there is a value at this level
+            if((level%2 == 0 && root->val <= levelPrev[level]) ||
+               (level%2 != 0 && root->val >= levelPrev[level]))
+                return false;
+        }
+        
+        levelPrev[level] = root->val;
+        
+        return solve(root->left, level+1) && solve(root->right, level+1);
+    }
+    
+    bool isEvenOddTree(TreeNode* root) {
+        return solve(root, 0);
+        
+    }
+};
